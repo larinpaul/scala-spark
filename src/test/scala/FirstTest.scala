bb38f1b379600f28e7fd2ee31fa925a838739c11
larinpaul <www.larinpavel@mail.ru>
2024-09-02 08:51:19 -0700

    // Instantiate a SparkSession private val spark = SparkSession.builder()   .appName("FirstTest")   .master("local[*]")   .getOrCreate()

--- src/test/scala/FirstTest.scala
+++ src/test/scala/FirstTest.scala
@@ -1,8 +1,16 @@
 package org.larinpaul.sparkdev
 
+import org.apache.spark.sql.SparkSession
 import org.scalatest.funsuite.AnyFunSuite
 
 class FirstTest extends AnyFunSuite {
+
+  // Instantiate a SparkSession
+  private val spark = SparkSession.builder()
+    .appName("FirstTest")
+    .master("local[*]")
+    .getOrCreate()
+
 
   test("add(2, 3) return 5") {
     Main.highestClosingPricesPerYear()
