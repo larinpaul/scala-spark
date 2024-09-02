e5dac7b64372639761a9c1bab177676e1511b173
larinpaul <www.larinpavel@mail.ru>
2024-09-01 19:52:16 -0700

    getting ready to test...

--- src/test/scala/FirstTest.scala
+++ src/test/scala/FirstTest.scala
@@ -5,13 +5,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class FirstTest extends AnyFunSuite {
 
   test("add(2, 3) return 5") {
-    val result = Main.add(2, 3)
-    assert(result == 5)
-  }
-
-  test("addFake(2, 3) return 5") {
-    val result = Main.addFake(2, 3)
-    assert(result == 5)
+    Main.highestClosingPricesPerYear()
   }
 
 
